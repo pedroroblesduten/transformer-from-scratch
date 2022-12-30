fbfa768037decfa637b9b4e588e8248e87716115
pedroroblesduten <pedroroblesduten@gmail.com>
2022-12-30 12:17:29 -0300

    attention modules

---
+++ attention_is_all_you_need/attention.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from helper import scaled_dot_product_attetion
+
+class AttentionHead(nn.Module)
+    def __init__(self, embedding_dim, head_dim):
+        super().__init__()
+
+        self.linear_Q = nn.Linear(embedding_dim, head_dim)
+        self.linear_K = nn.Linear(embedding_dim, head_dim)
+        self.linear_V = nn.Linear(embedding_dim, head_dim)
+
+    def forward(self, input):
+        one_head_attetion = scaled_dot_product_attetion(
+            self.linear_Q(input),
+            self.linear_K(input),
+            self.linear_V(input))
+
+        return one_head_attetion
+
+class MultiHeadAttetion(nn.Module):
+    def __init__(self, embedding_dim, n_heads, dropout=0.1):
+        super().__init__()
+        self.embedding_dim = embedding_dim
+        self.n_heads = n_heads
+        self.head_dim = embedding_dim // n_heads        
+        self.heads = nn.ModuleList([
+            AttentionHead(self.embedding_dim, self.head_dim)
+            for _ in range(self.n_heads)
+        ])
+        self.output_linear = nn.Linear(self.embedding_dim, self.embedding_dim)
+        
+    def forward(self, h):
+        x = torch.cat([head(h) for head in self.heads], dim=-1)
+        x = self.output_linear(x)
+        return x
+        
+
--- attention_is_all_you_need/helper.py
+++ attention_is_all_you_need/helper.py
@@ -1,1 +1,17 @@
 import torch
+import torch.nn as nn
+import torch.nn.functionall as F
+
+
+def scaled_dot_product_attetion(query, key, value, mask=None, dropout=None)
+    d_k = query.size(-1)
+    # Calculating Q*V/sqrt(d_k)
+    scores = torch.bmm(query, key.transpose(1, 2)) / sqrt(d_k)
+    if mask is not None:
+        scores = scores.masked_fill(mask==0, 1e-9)
+    p_attn = F.softmax(scores, dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    output = torch.bmm(p_attn, value)
+
+    return output, p_attn
